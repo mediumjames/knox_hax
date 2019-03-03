1a3e477cc7165cc4309e221062bb8d3b1544e13c
Russell Derby <rderby1@vols.utk.edu>
2019-03-02 20:29:54 -0500

    configured react

--- pothole-app/webpack.config.js
+++ pothole-app/webpack.config.js
@@ -2,17 +2,26 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
 module.exports = {
     mode: 'development',
     entry: './src/app.js',
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: DIST_DIR + "/app",
+        publicPath: "/app/"
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                },
                 exclude: /node_modules/
             }
         ]
